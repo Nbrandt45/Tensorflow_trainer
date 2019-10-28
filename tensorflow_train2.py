b5c218d1d7b4d654efac02e54c91f751db04047e
Nick Brandt <brandtn1@msu.edu>
2019-10-27 21:28:15 -0400

    Updated training model to account for Multi-classifications more than two

--- tensorflow_train2.py
+++ tensorflow_train2.py
@@ -1,12 +1,11 @@
 import tensorflow as tf
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import Dropout
-from tensorflow.keras.layers import Activation
-from tensorflow.keras.layers import Flatten
-from tensorflow.keras.layers import Conv2D
-from tensorflow.keras.layers import MaxPooling2D
+from tensorflow.keras.layers import Dense, Dropout, Activation, Flatten, Conv2D, MaxPooling2D
+from tensorflow.keras.optimizers import Adam
 import numpy as np
+from sklearn.datasets.samples_generator import make_blobs
+from tensorflow.keras.utils import to_categorical
+from matplotlib import pyplot
 
 
 data = np.load('data_features.npy')
@@ -14,6 +13,50 @@ data = np.load('data_features.npy')
 label = np.load('labels_list.npy')
 
 data = data/255.0
+
+# generate 2d classification dataset
+data, label = make_blobs(n_samples=1000, centers=3, n_features=2, cluster_std=2, random_state=2)
+# one hot encode output variable
+label = to_categorical(label)
+
+# split into train and test
+n_train = 500
+trainX, testX = data[:n_train, :], data[n_train:, :]
+trainy, testy = label[:n_train], label[n_train:]
+
+# define model
+model = Sequential()
+model.add(Dense(50, input_dim=2, activation='relu', kernel_initializer='he_uniform'))
+model.add(Dense(3, activation='softmax'))
+
+# compile model
+opt = Adam(lr=0.01)
+model.compile(loss='categorical_crossentropy', optimizer=opt, metrics=['accuracy'])
+
+# fit model
+history = model.fit(trainX, trainy, validation_data=(testX, testy), epochs=100, verbose=2)
+
+# evaluate the model
+_, train_acc = model.evaluate(trainX, trainy, verbose=0)
+_, test_acc = model.evaluate(testX, testy, verbose=0)
+print('Train: %.3f, Test: %.3f' % (train_acc, test_acc))
+
+# plot loss during training
+pyplot.subplot(211)
+pyplot.title('Loss')
+pyplot.plot(history.history['loss'], label='train')
+pyplot.plot(history.history['val_loss'], label='test')
+pyplot.legend()
+
+# plot accuracy during training
+pyplot.subplot(212)
+pyplot.title('Accuracy')
+pyplot.plot(history.history['accuracy'], label='train')
+pyplot.plot(history.history['val_accuracy'], label='test')
+pyplot.legend()
+pyplot.show()
+
+"""
 
 model = Sequential()
 model.add(Conv2D(64, (3, 3), input_shape=data.shape[1:]))
@@ -30,9 +73,11 @@ model.add(Dense(1))
 
 model.add(Activation('sigmoid'))
 
-model.compile(loss="binary_crossentropy", optimizer="adam", metrics=['accuracy'])
+opt = Adam(lr=0.01)
+
+model.compile(loss="categorical_crossentropy", optimizer=opt, metrics=['accuracy'])
 
 model.fit(data, label, batch_size=90, epochs=20, verbose=1, validation_split=0.1)
 
-
+"""
 
